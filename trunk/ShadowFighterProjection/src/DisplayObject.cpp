58c66849003c86d081b034ae1fa67200fa38d297
peteruithoven <peteruithoven@d00d9dc6-188e-b831-f797-75abec932b65>
2010-04-16 21:13:26 +0000

    A separate application that we will use for projection. Separate so that we can still debug on the laptop.

---
+++ trunk/ShadowFighterProjection/src/DisplayObject.cpp
@@ -0,0 +1,26 @@
+/*
+ *  DisplayObject.cpp
+ *  openFrameworks
+ *
+ *  Created by Peter Uithoven on 5/8/09.
+ *  Copyright 2009 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "DisplayObject.h"
+
+DisplayObject::DisplayObject()
+{
+	x = 0;
+	y = 0;
+	alpha = 1;
+	width = 0;
+	height = 0;
+	rotation = 0;
+	ofAddListener(ofEvents.draw, this, &DisplayObject::draw);
+}
+
+void DisplayObject::draw(ofEventArgs & args)
+{
+
+}

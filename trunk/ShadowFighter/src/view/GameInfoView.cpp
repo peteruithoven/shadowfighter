466b11cd6516abd820b34941f75f19f37ad70752
peteruithoven <peteruithoven@d00d9dc6-188e-b831-f797-75abec932b65>
2010-04-26 00:09:23 +0000

    Forgot to add these (Missing a Versions auto add system)

---
+++ trunk/ShadowFighter/src/view/GameInfoView.cpp
@@ -0,0 +1,48 @@
+/*
+ *  GameInfoView.cpp
+ *  ShadowFighter
+ *
+ *  Created by Peter Uithoven on 4/24/10.
+ */
+
+#import "GameInfoView.h"
+
+
+GameInfoView::GameInfoView()
+{
+	
+}
+
+void GameInfoView::setModel(Model * model)
+{
+	this->model = model;
+	ofAddListener(model->HIT,this,&GameInfoView::onHit);
+	ofAddListener(model->RESET,this,&GameInfoView::onReset);
+	
+	int videoW = model->videoW;
+	
+	
+	//healthBarPlayer1.x = (videoW/2-healthBarPlayer1.width)/2;
+	//healthBarPlayer2.x = (videoW/2-healthBarPlayer2.width)/2+videoW/2;
+	int marginX = (videoW-healthBarPlayer1.width-healthBarPlayer2.width)/3;
+	healthBarPlayer1.x = marginX;
+	healthBarPlayer2.x = videoW-healthBarPlayer2.width-marginX;
+	healthBarPlayer1.y = healthBarPlayer2.y = 10;
+	
+	update();
+}
+void GameInfoView::onHit(int & arg)
+{
+	cout << "GameInfoView::onHit\n";
+	update();
+}
+void GameInfoView::onReset(int & arg)
+{
+	cout << "GameInfoView::onReset\n";
+	update();
+}
+void GameInfoView::update()
+{
+	healthBarPlayer1.percentage = float(model->player1Health)/model->startHealth;
+	healthBarPlayer2.percentage = float(model->player2Health)/model->startHealth;
+}

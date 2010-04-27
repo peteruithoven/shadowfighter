d32f4a18e716a13e29a51f50409fa8d17749f47b
peteruithoven <peteruithoven@d00d9dc6-188e-b831-f797-75abec932b65>
2010-04-27 22:20:25 +0000

    Receives health info Added healthbars Receives states Shows victory screen Only shows health bars on game state Tweaked position and scale of hit indicators Added Boom images as tryout. Animated Boom images as tryout.

---
+++ ShadowFighterProjection/src/HealthBar.h
@@ -0,0 +1,26 @@
+/*
+ *  HealthBar.h
+ *  ShadowFighter
+ *
+ *  Created by Peter Uithoven on 4/24/10.
+ */
+
+#ifndef _HealthBar
+#define _HealthBar
+
+#include "ofMain.h"
+#include "ofEvents.h"
+#include "DisplayObject.h"
+
+class HealthBar : public DisplayObject 
+{
+	public:
+		float percentage;
+		HealthBar();
+		void draw();
+	
+	private:
+
+};
+
+#endif

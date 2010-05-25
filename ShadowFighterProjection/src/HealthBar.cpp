6fdc09b6a430649449d0ef40a80ac0f4fe7de055
peteruithoven <peteruithoven@d00d9dc6-188e-b831-f797-75abec932b65>
2010-05-25 08:06:07 +0000

    - Healthbar - organized images

--- ShadowFighterProjection/src/HealthBar.cpp
+++ ShadowFighterProjection/src/HealthBar.cpp
@@ -13,18 +13,44 @@ HealthBar::HealthBar()
 	percentage = 1;
 	width = 150;
 	height = 30;
-	
+	 
 	setAutoDraw(false);
+	
+	bgImg.loadImage("images/healthbar/healthbar_bg_red.png");
+	barImgOrg.loadImage("images/healthbar/healthbar_fg_yellow.png");
+	height = bgImg.height;
+	width = bgImg.width;
 }
 
 void HealthBar::draw()
 {
-	ofNoFill();
-	ofSetColor(0xffffff);
-	ofSetLineWidth(2);
-	ofRect(x, y, width, height);
-	ofSetLineWidth(1);
+//	ofNoFill();
+//	ofSetColor(0xffffff);
+//	ofSetLineWidth(2);
+//	ofRect(x, y, width, height);
+//	ofSetLineWidth(1);
+//	
+//	ofFill();
+//	ofRect(x, y, width*percentage, height);
 	
-	ofFill();
-	ofRect(x, y, width*percentage, height);
+	unsigned char *pixels = barImgOrg.getPixels();
+	
+	int borderWidth = 7;
+	int barWidth = (width-borderWidth*2)*percentage + borderWidth;
+	
+	for (int ix = barWidth; ix < width; ix++) 
+	{
+		for (int iy = 0; iy < height; iy++) 
+		{
+			int pixelIndex = ( (iy*width) + ix ) *4;
+			pixels[pixelIndex+3] = 0; //a
+		}
+	}
+	ofImage barImg;
+	barImg.setFromPixels(pixels, width, height, OF_IMAGE_COLOR_ALPHA, true);
+	ofEnableAlphaBlending();
+	bgImg.draw(x, y);
+	barImg.draw(x, y);
+	ofDisableAlphaBlending();
+	
 }
